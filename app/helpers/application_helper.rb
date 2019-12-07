4bec63c9d935c1c1bbb82922317ba89f919cf5ee
kawa1228 <sxa.1105bo@gmail.com>
2019-12-07 10:32:52 +0900

    titleを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  # ページごとの完全なタイトルを返します
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+end
+(1..5).each do |number|
+   puts 2 * number
+   puts '--a
 end
